53eedf2985c918ff97157c01cd819d2611e88590
TomasR <Rakusan2@gmail.com>
2017-03-07 13:50:52 -0500

    add fs promise and move key to file

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-let key = "xoxb-147371785172-lzTFS1mRXSzVAfFs7HJrWkas";
+const fs = require("fs");
 const slack = require("slack");
+let keyFS = fsPromise('key.txt');
 const tbaApi_1 = require("./tbaApi");
-tbaApi_1.TBAReq.Status().then(status => {
+let year = 2017;
+Promise.all([tbaApi_1.TBAReq.Status(), keyFS]).then(([status, key]) => {
+    if (status.is_datafeed_down) {
+        console.log('datafeed is down');
+        console.log(status);
+        return;
+    }
+    year = status.current_season;
     let bot = slack.rtm.client();
     bot.listen({ token: key });
     bot.hello(msg => console.log(msg));
@@ -13,50 +21,70 @@ tbaApi_1.TBAReq.Status().then(status => {
         console.log({ a });
         if (a.text[0] === '!') {
             let c = a.text.split(' ');
-            switch (c[0].toLowerCase()) {
-                case '!info':
-                    let num = /\d{1,4}/.exec(c[1]);
-                    if (num !== null) {
-                        Promise.all([tbaApi_1.TBAReq.TeamReq('frc' + num[0]), tbaApi_1.TBAReq.TeamEvents('frc' + num[0], status.current_season)]).then(([team, events]) => {
-                            slack.chat.postMessage({
-                                token: key, channel: a.channel, text: `Info for Team ` + num[0], attachments: [
-                                    {
-                                        title: "Info",
-                                        fields: [
-                                            {
-                                                title: "Team Name",
-                                                value: team.nickname
-                                            },
-                                            {
-                                                title: "Rookie Year",
-                                                value: team.rookie_year.toString()
-                                            },
-                                            {
-                                                title: "Current Competitions",
-                                                value: events.map(a => a.start_date + ' -> ' + a.short_name + ' in ' + a.location).sort().join('\n') || "This team is not participating in any events this year"
-                                            }
-                                        ]
-                                    }
-                                ]
-                            }, (err, data) => {
-                                console.log({ err, data });
-                            });
-                        }).catch(err => {
-                            if ('404' in err) {
-                                slack.chat.postMessage({ token: key, channel: a.channel, text: `Sorry team ${c[1]} does not exist ${parseInt(c[1]) > 7000 ? 'yet' : ''}` }, (err, data) => {
-                                    console.log({ err, data });
-                                });
-                            }
-                            console.log({ err });
-                        });
-                    }
-                    else {
-                        slack.chat.postMessage({ token: key, channel: a.channel, text: 'Sorry That is an invalid team number' }, (err, data) => {
-                            console.log({ err, data });
-                        });
-                    }
-                    break;
+            let com = c[0].toLowerCase();
+            if (com in commands) {
+                commands[com](a, c, res => {
+                    let post = { token: key, channel: a.channel, text: res.text };
+                    if (res.attachments)
+                        post.attachments = res.attachments;
+                    slack.chat.postMessage(post, (err, data) => {
+                        console.log({ err, data });
+                    });
+                });
             }
         }
     });
 }).catch(e => console.log(e));
+let commands = {
+    "!info": (mesg, par, res) => {
+        let num = /\d{1,4}/.exec(par[1]);
+        if (num !== null) {
+            Promise.all([tbaApi_1.TBAReq.TeamReq('frc' + num[0]), tbaApi_1.TBAReq.TeamEvents('frc' + num[0], year)]).then(([team, events]) => {
+                res({
+                    text: `Info for Team ` + num[0],
+                    attachments: [
+                        {
+                            title: "Info",
+                            fields: [
+                                {
+                                    title: "Team Name",
+                                    value: team.nickname
+                                },
+                                {
+                                    title: "Rookie Year",
+                                    value: team.rookie_year.toString()
+                                },
+                                {
+                                    title: "Current Competitions",
+                                    value: events.map(a => a.start_date + ' -> ' + a.short_name + ' in ' + a.location).sort().join('\n') || "This team is not participating in any events this year"
+                                }
+                            ]
+                        }
+                    ]
+                });
+            }).catch(err => {
+                if ('404' in err) {
+                    res({ text: `Sorry team ${par[1]} does not exist ${parseInt(par[1]) > 7000 ? 'yet' : ''}` });
+                }
+                console.log({ err });
+            });
+        }
+        else {
+            res({ text: 'Sorry That is an invalid team number' });
+        }
+    },
+    "!status": (mesg, par, res) => {
+    },
+    "!track": (mesg, par, res) => {
+    }
+};
+function fsPromise(src) {
+    return new Promise((res, rej) => {
+        fs.readFile(src, 'utf8', (err, data) => {
+            if (err)
+                rej(err);
+            else
+                res(data);
+        });
+    });
+}
